a41bcb94e6142da75b5c2964555fca34401613d0
Mario Gilezan <mariogilezan10@gmail.com>
2023-03-20 21:55:29 +0100

    organize variables and implement load and close pop-up form with overlay

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
+// Base variables
+const body = document.body;
 const currentUrl = window.location.href;
-const headerLinks = document.querySelectorAll('.header__link');
 
+// Header variables
+const headerLinks = document.querySelectorAll('.header__link');
+const header = document.querySelector('.header');
+const headerList = document.querySelector('.header__list');
+const menuToggle = document.querySelector('.header__menuToggle');
+
+// Overlay variables
+const overlay = document.querySelector('.overlay');
+const overlayContact = document.querySelector('.overlay__contact');
+const closeOverlayBtn = document.querySelector('.overlay__close');
+const cancelOverlayBtn = document.querySelector('.overlay__btn--cancel');
+
+// Handle active navigation link
 headerLinks.forEach((link) => {
   if (link.href === currentUrl) {
     link.classList.add('active');
   }
 });
 
-const body = document.body;
-const header = document.querySelector('.header');
-const headerList = document.querySelector('.header__list');
-const menuToggle = document.querySelector('.header__menuToggle');
-
+// Toggle mobile menu
 menuToggle.addEventListener('click', () => {
   headerList.classList.toggle('open');
   body.classList.toggle('no-scroll');
@@ -25,4 +35,26 @@ window.addEventListener('resize', () => {
     headerList.classList.remove('open');
     body.classList.remove('no-scroll');
   }
+});
+
+// Load overlay on page load
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    overlay.style.opacity = 1;
+    overlay.style.visibility = 'visible';
+    overlay.style.transition = 'all 1s ease-out';
+    overlayContact.style.transform = 'translateY(0)';
+    overlayContact.style.transition = 'transform 2s ease-out';
+  }, 1000);
+});
+
+// Handle close overlay
+[closeOverlayBtn, cancelOverlayBtn].forEach((btn) => {
+  btn.addEventListener('click', () => {
+    overlay.style.opacity = 0;
+    overlay.style.visibility = 'hidden';
+    overlay.style.transition = 'all 1s ease-in';
+    overlayContact.style.transform = 'translateY(-200%)';
+    overlayContact.style.transition = 'transform 1s ease-in';
+  });
 });
